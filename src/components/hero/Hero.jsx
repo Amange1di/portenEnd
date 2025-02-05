5acc41e15d02883af4b406f8df5aa1acb374cf81
Amange1di <amangeldi2702006@gmail.com>
2025-02-05 12:49:32 +0600

    modified:   src/components/hero/hero.scss

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,7 +4,6 @@ import logo from '../.././shared/assets/logo.png'
 import { useTranslation } from "react-i18next";
 const herodata = [
   {
-    
     p: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus interdum purus, est tortor pulvinar ut in. Fringilla a diam enim sed justo, sed iaculis sagittis. Tortor id eu interdum nec ut iaculis. Penatibus ullamcorper ultricies morbi ipsum sem metus pharetra, mi. Tortor nibh magna feugiat id nunc, dui nisl viverra.",
     logo: "logo",
     t:"Saint Petersburg"
